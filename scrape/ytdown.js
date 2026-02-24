62d8e1aba44fb28af91f2a9d150137fca5979816
Agus <davidmersey707@gmail.com>
2026-02-24 12:09:16 +0800

    Fix API endpoint and enhance error handling
    Updated API endpoint and added optional chaining for safety.

--- scrape/ytdown.js
+++ scrape/ytdown.js
@@ -3,20 +3,20 @@ import { delay } from 'baileys';
 //const delay = ms => new Promise(r => setTimeout(r, ms));
 
 async function ytdown(url, type = 'video') {
-	const { data } = await axios.post('https://ytdown.to/proxy.php', new URLSearchParams({ url }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
+	const { data } = await axios.post('https://app.ytdown.to/proxy.php', new URLSearchParams({ url }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
 
 	const api = data.api;
+	if (api?.status == 'ERROR') throw new Error(api.message);
 
-	const media = api.mediaItems.find((m) => m.type.toLowerCase() === type.toLowerCase());
-
+	const media = api?.mediaItems?.find((m) => m.type.toLowerCase() === type.toLowerCase());
 	if (!media) throw new Error('Media type not found');
 
 	while (true) {
 		const { data: res } = await axios.get(media.mediaUrl);
 
-		if (res.error === 'METADATA_NOT_FOUND') throw new Error('Metadata not found');
+		if (res?.error === 'METADATA_NOT_FOUND') throw new Error('Metadata not found');
 
-		if (res.percent === 'Completed' && res.fileUrl !== 'In Processing...') {
+		if (res?.percent === 'Completed' && res?.fileUrl !== 'In Processing...') {
 			return {
 				info: {
 					title: api.title,
